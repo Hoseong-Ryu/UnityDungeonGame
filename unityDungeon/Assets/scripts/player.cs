bc65a57d71368e24005cef7df498b3bd2d920a2a
Hoseong-Ryu <52982845+Hoseong-Ryu@users.noreply.github.com>
2020-11-04 09:39:43 +0900

    base_attack
    기본 공격

---
+++ unityDungeon/Assets/scripts/player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.Play("bk_rh_right_A", -1, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.Play("hk_side_left_A", -1, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.Play("hp_straight_A", -1, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.Play("hp_straight_right_A", -1, 0);
+        }
+    }
+}
