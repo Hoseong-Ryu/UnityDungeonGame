96603226b563eef0f7950ac69f36115075e0d2b6
eunjongyeop <48788795+eunjongyeop@users.noreply.github.com>
2020-11-11 10:33:17 +0900

    add movement
    걷기, 달리기, 뛰기 에니메이션 추가
    캐릭터가 움직임

--- unityDungeon/Assets/scripts/player.cs
+++ unityDungeon/Assets/scripts/player.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public Animator animator;
+    private CharacterController controller;
+    private bool dir = true;  // 오른쪽 : true, 왼쪽 false
+    public float speed = 5;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -20,19 +24,83 @@ public class player : MonoBehaviour
             animator.Play("bk_rh_right_A", -1, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.Z))
+        else if (Input.GetKeyDown(KeyCode.Z))
         {
             animator.Play("hk_side_left_A", -1, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A))
         {
             animator.Play("hp_straight_A", -1, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S))
         {
             animator.Play("hp_straight_right_A", -1, 0);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            animator.SetBool("walking", true);
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walk") && dir)
+            {
+                animator.Play("Run", -1, 0);
+            } else
+            {
+                animator.Play("Walk", -1, 0);
+            }
+
+            if (!dir)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                dir = true;
+            }
+        }
+
+        else if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            if (!Input.GetKey(KeyCode.LeftArrow))
+                animator.SetBool("walking", false);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            animator.SetBool("walking", true);
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walk") && !dir)
+            {
+                animator.Play("Run", -1, 0);
+            }
+            else
+            {
+                animator.Play("Walk", -1, 0);
+            }
+
+            if (dir)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                dir = false;
+            }
+        }
+
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            if(!Input.GetKey(KeyCode.RightArrow))
+                animator.SetBool("walking", false);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.Play("Jump");
+        }
+
+        // move player
+        if (animator.GetBool("walking"))
+        {
+            float speed = this.speed;
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Run"))
+                speed *= 2;
+
+            controller.Move(Time.deltaTime * speed * transform.TransformDirection(Vector3.forward));
         }
     }
 }
