f6cedca7bb2e2c58420febdce0c7051c772e06bc
류호성 <52982845+Hoseong-Ryu@users.noreply.github.com>
2020-12-08 15:35:25 +0900

    Camera tracking

---
+++ unityDungeon/Assets/scripts/Cam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class Cam : MonoBehaviour {
+ 
+    public GameObject target;
+ 
+    public float offsetX;
+    public float offsetY;
+    public float offsetZ;
+ 
+    public float DelayTime;
+ 
+    // Update is called once per frame
+    void Update () {
+        var position = target.transform.position;
+        Vector3 FixedPos =
+            new Vector3(
+                position.x + offsetX,
+                position.y + offsetY,
+                position.z + offsetZ);
+        transform.position = Vector3.Lerp(transform.position, FixedPos, Time.deltaTime * DelayTime);
+    }
+}
